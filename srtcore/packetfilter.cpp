f0c8b88e97be4f56e8968870bc0e68c0b6957398
Yaroslav Molochko <onorua@gmail.com>
2025-06-29 23:16:12 +0300

    udpspeeder -> rsfec

--- srtcore/packetfilter.cpp
+++ srtcore/packetfilter.cpp
@@ -316,8 +316,8 @@ PacketFilter::Internal::Internal()
     m_filters["fec"] = new PacketFilter::Creator<FECFilterBuiltin>;
     m_builtin_filters.insert("fec");
 
-    m_filters["udpspeeder"] = new PacketFilter::Creator<UDPspeederFilter>;
-    m_builtin_filters.insert("udpspeeder");
+    m_filters["rsfec"] = new PacketFilter::Creator<RSFECFilter>;
+    m_builtin_filters.insert("rsfec");
 }
 
 bool PacketFilter::configure(CUDT* parent, CUnitQueue* uq, const std::string& confstr)
